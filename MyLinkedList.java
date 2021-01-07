ec9c3b0d0016207eb3e26f09caeea1a6494c80af
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2021-01-06 21:23:28 -0500

    get, add at index

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -24,9 +24,40 @@ public class MyLinkedList{
     }
     return true;
   }
-  // public boolean add(int index, String value);
-  // public String get(int index);
+
+  public void add(int index, String value) throws IndexOutOfBoundsException {
+    if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+    else if (index == size) add(value);
+    else {
+      Node n = start;
+      for (int i = 0; i < index; i ++) {
+        n = n.getNext();
+        if (n == null)  throw new IndexOutOfBoundsException();
+      }
+
+    Node prev = n.getPrev();
+    Node next = n.getNext();
+
+    Node a = new Node(value);
+    
+    a.setPrev(n.getPrev());
+    a.setNext(n.getNext());
+
+    prev.setNext(a);
+    next.setPrev(a);
+
+    }
+  }
+
+  public String get(int index) {
+    Node n = start;
+    for (int i = 0; i < index; i ++) {
+      n = start.getNext();
+      if (n == null) throw new IndexOutOfBoundsException();
+    }
+    return n.getData();
+  }
   // public String set(int index, String value);
   // public String toString();
-  //Any helper method that returns a Node object MUST BE PRIVATE!
+  // Any helper method that returns a Node object MUST BE PRIVATE!
 }
